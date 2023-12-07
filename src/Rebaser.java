b408a262c519ac67f7dc19075990f9663fef8044
rosxie <75559060+Axion747@users.noreply.github.com>
2023-12-07 08:46:43 -0500

    Base Project Setup

---
+++ src/Rebaser.java
@@ -0,0 +1,43 @@
+
+public class Rebaser {
+
+    private String storedValue;
+    public Rebaser(){
+        storedValue = "0";
+    }
+
+    public Rebaser(String value){
+        this.storedValue = value;
+    }
+
+    /**
+     * Changes the stored value to the user-input value.
+     * @param value Non-negative integer in base 16 or less.
+     */
+    public void setValue(String value){
+
+    }
+
+    /**
+     * Retrieves stored value.
+     * @return Stored value as a string.
+     */
+    public String getValue(){
+
+    }
+
+    /**
+     * Converts stored value (assumed in base 10) to a base n.
+     * @param n The base value to be converted to.
+     * @return The stored value in base n as a String.
+     */
+
+    public String convertToBaseN(int n){
+
+    }
+
+    public String convertToBase10(int n){
+
+    }
+
+}
