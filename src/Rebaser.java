d8f9a7f2fe6c2a03a9e597c0c65309775374782a
rosxie <benson.chi.zhang@gmail.com>
2023-12-14 08:16:08 -0500

    Updated a verify input private method

--- src/Rebaser.java
+++ src/Rebaser.java
@@ -1,7 +1,10 @@
-
+import java.util.Arrays;
+import java.lang.Math;
 public class Rebaser {
 
     private String storedValue;
+    private static final char[] LIST_OF_INPUTS = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'};
+    private static final String LIST_INPUTS = "0123456789ABCDEF";
     public Rebaser(){
         storedValue = "0";
     }
@@ -10,6 +13,23 @@ public class Rebaser {
         this.storedValue = value;
     }
 
+    private boolean verifyInput(String input){
+        if (input.isEmpty()){
+            return false;
+        }
+        String newStr = input.toUpperCase();
+        int p = 1;
+        boolean ans;
+        for (int i = 0; i < input.length(); i++){
+            if (LIST_INPUTS.contains(newStr.substring(i, i+1))){
+                p *= 1;
+            }
+            else{
+                p *= 0;
+            }
+        }
+        return ((p == 1) ? (ans = true) : (ans = false));
+    }
     /**
      * Changes the stored value to the user-input value.
      * @param value Non-negative integer in base 16 or less.
@@ -23,7 +43,7 @@ public class Rebaser {
      * @return Stored value as a string.
      */
     public String getValue(){
-
+        return ""; // place holder
     }
 
     /**
@@ -33,11 +53,15 @@ public class Rebaser {
      */
 
     public String convertToBaseN(int n){
-
+        return ""; // placeholder
     }
 
     public String convertToBase10(int n){
+        return ""; // placeholder
+    }
 
+    // Just for testing purposes
+    public static void main (String[] args){
     }
 
 }
