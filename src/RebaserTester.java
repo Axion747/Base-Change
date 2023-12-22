f5bda64df9d63a4a8d29a080a78f7331e8048cf8
rosxie <benson.chi.zhang@gmail.com>
2023-12-21 20:36:40 -0500

    Everything *should* be working. Hopefully. :)

--- src/RebaserTester.java
+++ src/RebaserTester.java
@@ -2,10 +2,10 @@ public class RebaserTester {
     public static void main(String[] args){
 //        System.out.println("Not completed. :)");
 //        System.out.println(12/11);
-        Rebaser test = new Rebaser("11");
-        System.out.println(test.convertToBase10(10));
+        Rebaser test = new Rebaser("187236");
+        System.out.println(test.convertToBase10(9));
 //        System.out.println(test.getValue());
 //        test.setValue("BF");
-//        System.out.println(test.convertToBaseN(12));
+        System.out.println(test.convertToBaseN(12));
     }
 }
